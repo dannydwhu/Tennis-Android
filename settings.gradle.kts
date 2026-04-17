e0f5e0414dd4082a0912f71f68d8450c2237f521
hudawei <347614241@qq.com>
2026-04-17 23:58:17 +0800

    fix gradle version issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,10 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
         google()
         mavenCentral()
     }
 }
-
-rootProject.name = "SmartformTennis"
-include(":app")
